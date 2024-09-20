30974d9165f07637d307ea1fdfb994a3c637b534
Fernando Romero <romerocruzfernando@gmail.com>
2024-09-19 23:45:57 -0600

    Correción interfaz

--- src/Nodo.py
+++ src/Nodo.py
@@ -8,27 +8,7 @@ class Nodo:
     """
     def __init__(self, id_nodo: int, vecinos: list, canal_entrada: simpy.Store,
                  canal_salida: simpy.Store):
-        self.id_nodo = id_nodo
-        self.vecinos = vecinos
-        self.canal_entrada = canal_entrada
-        self.canal_salida = canal_salida
+        """Inicializa los atributos del nodo."""
 
-    """
-    Método de acceso del identificador de un nodo
-    """
-    def get_id(self) -> int: return self.id
-
-    """
-    Método de acceso a la lista de vecinos del nodo
-    """
-    def get_vecinos(self) -> list: return self.vecinos
-
-    """
-    Método de acceso al canal de entrada del nodo
-    """
-    def get_canal_entrada(self) -> simpy.Store: return self.canal_entrada
-
-    """
-    Método de acceso al canal de salida del nodo
-    """
-    def get_canal_salida(self) -> simpy.Store: return self.canal_salida
+    def get_id(self) -> int:
+        """Regresa el id del nodo."""
